c6196580d779dfb419806af8a1378f7eeeefeb2a
unknown <ttddcc119@naver.com>
2024-04-05 13:30:48 +0900

    외부 CSS painting 관련 속성 모두 제거

--- client/src/assets/style/Theme.js
+++ client/src/assets/style/Theme.js
@@ -1,11 +1,3 @@
-const light = {
-  Title1: '#010E26',
-  Title2: '#263973',
-  Title3: '#6B7FF2',
-  borderLine: '#aaa',
-  backgroundColor: '#FAFAFA',
-};
-
 const Theme = {
   // TODO DarkMode 스타일 나중에 채우기
   Dark: {
--- client/src/hooks/useFirstTheme.jsx
+++ client/src/hooks/useFirstTheme.jsx
@@ -3,19 +3,20 @@ import { useState, useEffect } from 'react';
 import Theme from '../assets/style/Theme';
 
 /**
+ * ? 굳이 themeStatus 까지 나눠서 상태를 정의해야 하나 ? 그냥 직접적으로 theme 를 바꿔주면 안되나 ?
  * useFirstTheme 은 첫 렌더링 시 ThemeProvider 에게 기본 테마 값을 지정해주는 커스텀 훅
- * 만약 로컬 스토리지에 저장된 값이 없다면 로컬 스토리지에 값을 추가하고 light theme 로 설정
- * @const {String} thmeStatus - light , dark 중 하나의 값을 의미
+ * 만약 로컬 스토리지에 저장된 값이 없다면 로컬 스토리지에 값을 추가하고 Light theme 로 설정
+ * @const {String} thmeStatus - Light , Dark 중 하나의 값을 의미
  * @returns {Object} - themeStatus 에 따른 테마 팔레트를 반환
  */
 const useFirstTheme = () => {
-  const [themeStatus, setTheme] = useState('light');
+  const [themeStatus, setTheme] = useState('Light');
   const theme = Theme[themeStatus];
 
   useEffect(() => {
     const prevTheme = window.localStorage.getItem('themeStatus');
-    setTheme(prevTheme || 'light');
-    if (!prevTheme) window.localStorage.setItem('themeStatus', 'light');
+    setTheme(prevTheme || 'Light');
+    if (!prevTheme) window.localStorage.setItem('themeStatus', 'Light');
   }, []);
 
   return { theme, setTheme };
