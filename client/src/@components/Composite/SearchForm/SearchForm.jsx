966907356c0f5a2cf3c5bf7689c5964777b71b5b
unknown <ttddcc119@naver.com>
2024-04-08 16:52:23 +0900

    SearchButton 에서 inputRef.current.value 에 접근할 때 null 에 접근하는 이유

--- client/src/@components/Composite/SearchForm/SearchForm.jsx
+++ client/src/@components/Composite/SearchForm/SearchForm.jsx
@@ -8,22 +8,30 @@ import moduleCss from './SearchForm.module.css';
 
 // import customHooks
 import useSearchRef from '../../../hooks/useSearchRef';
-
+import useTranslation from '../../../hooks/useTranslation';
 const SearchForm = ({ children }) => {
-  return <Form className={moduleCss.searchForm}>{children}</Form>;
+  const { error, isLoading } = useTranslation;
+  console.log(error, isLoading);
+
+  return (
+    <Form className={moduleCss.searchForm}>
+      {isLoading && <p>로딩중입니다</p>}
+      {error && <p>에러가 발생했습니다</p>}
+      {!isLoading && children}
+    </Form>
+  );
 };
 
 const SearchButton = () => {
-  // TODO onClick , item , className 채우기
   const inputRef = useSearchRef();
+  // fetchLatLong 함수는 locationString 을 기억하는 클로저 함수
+  const { fetchLatLong } = useTranslation(inputRef.current.value);
 
   return (
     <Button
       item='click me !'
       className={moduleCss.searchButton}
-      onClick={() => {
-        console.log(inputRef.current.value);
-      }}
+      // onClick={}
     />
   );
 };
