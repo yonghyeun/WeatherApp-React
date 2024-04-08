e88955838962769a4b0478f1844127d21f34bbbf
unknown <ttddcc119@naver.com>
2024-04-08 15:35:30 +0900

    useTranslation 은 어디에서 호출되어야 할까

--- client/src/hooks/useTranslation.jsx
+++ client/src/hooks/useTranslation.jsx
@@ -1,15 +1,22 @@
 import { useState } from 'react';
 import { APIKEY, URI } from '../@constants/_API';
 
-const useLocation = (query) => {
-  const encodeQuery = encodeURIComponent(query);
+/**
+ * 주어진 주소를 사용하여 위도와 경도를 조회하는 훅
+ *
+ * @param {String} location - 도로명 , 번지수 주소로 적힌 주소
+ * @returns {Object} - 훅 내부 상태를 변경시키는 함수와 상태들을 담은 객체
+ * @returns {Function} fetchingLatLong - 카카오 API 를 이용하여 위도 , 경도로 변경하는 함수
+ */
+const useTranslation = (location) => {
+  const encodeQuery = encodeURIComponent(location);
   const ENDPOINT = `${URI}/?query=${encodeQuery}`;
 
-  const [data, setData] = useState(null);
+  const [LatLong, setLatLong] = useState(null);
   const [error, setError] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
 
-  const fetchLocation = async () => {
+  const fetchLatLong = async () => {
     try {
       setIsLoading(true);
       const response = await fetch(ENDPOINT, {
@@ -22,7 +29,7 @@ const useLocation = (query) => {
         throw new Error('카카오 API 네트워크 상태가 불안정합니다');
 
       const json = await response.json();
-      setData(json);
+      setLatLong(json);
     } catch (error) {
       setError(error);
     } finally {
@@ -31,11 +38,11 @@ const useLocation = (query) => {
   };
 
   return {
-    fetchLocation,
-    data,
+    fetchLatLong,
+    LatLong,
     error,
     isLoading,
   };
 };
 
-export default useLocation;
+export default useTranslation;
--- client/src/layouts/ContentLayout/ContentHeader/ContentHeader.jsx
+++ client/src/layouts/ContentLayout/ContentHeader/ContentHeader.jsx
@@ -6,7 +6,6 @@ import TempButton from '../../../@components/UI/TempButton/TempButton';
 
 import useTheme from '../../../hooks/useTheme';
 
-// TODO 내부에 컴포넌트들 집어넣기
 const ContentHeader = () => {
   const { theme } = useTheme();
   return (
