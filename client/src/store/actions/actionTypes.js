6ff73b15cad9683630c27538a2331bb0a573a0f2
unknown <ttddcc119@naver.com>
2024-04-11 18:28:33 +0900

    actionTypes 간소화

--- client/src/store/actions/actionTypes.js
+++ client/src/store/actions/actionTypes.js
@@ -1,10 +1,5 @@
-// API Status 를 위한 action Types
-const OK = 'OK';
-const LOADING = 'LOADING';
-const ERROR = 'ERROR';
-
 // DATA Fetching 을 위한 action Types
 const SEARCH_LOCATION = 'SEARCH_LODATION';
 const FETCHING_DATA = 'FETCHING_DATA';
 
-export { OK, LOADING, ERROR, SEARCH_LOCATION, FETCHING_DATA };
+export { SEARCH_LOCATION, FETCHING_DATA };
--- client/src/store/reducers/StatusReducer.js
+++ client/src/store/reducers/StatusReducer.js
@@ -1,7 +1,3 @@
-import { OK, LOADING, ERROR } from '../actions/actionTypes';
-
-// statusReducer 는 너무 단순하네 ..
-
 const statusReducer = (state, action) => ({ ...state, status: action.type });
 
 export default statusReducer;
