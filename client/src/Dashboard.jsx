324a1e77b954cf31585b34a82e21334d1fe3d2b1
unknown <ttddcc119@naver.com>
2024-04-11 19:01:30 +0900

    테마 버튼 redux 로 수정 시작

--- client/src/Dashboard.jsx
+++ client/src/Dashboard.jsx
@@ -1,24 +1,30 @@
 // import Context
 import { ThemeProvider } from './context/ThemeProvider';
+import { Provider } from 'react-redux';
+
 // import Component
 import DashboardWrapper from './@components/UI/DashboardWrapper/DashboardWrapper';
 // import Layout
 import SidebarLayout from './layouts/SideBarLayout/SidebarLayout';
 import ContentLayout from './layouts/ContentLayout/ContentLayout';
 // import style
-
 import './index.css';
+// import store
+import store from './store/store';
+
 const Dashboard = () => {
   // TODO react-router-dom 계층 만들고 NavLink 로 변경하기
   // TODO Sidebar 합성 컴포지션으로 변경 할 수 있도록 리팩토링 공부하기
 
   return (
-    <ThemeProvider>
-      <DashboardWrapper>
-        <SidebarLayout />
-        <ContentLayout />
-      </DashboardWrapper>
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider>
+        <DashboardWrapper>
+          <SidebarLayout />
+          <ContentLayout />
+        </DashboardWrapper>
+      </ThemeProvider>
+    </Provider>
   );
 };
 
