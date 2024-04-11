0d5d410ca26be66005f9426521affd21b79b5fb9
unknown <ttddcc119@naver.com>
2024-04-11 18:26:19 +0900

    StatusReducer 수정

--- client/src/store/reducers/StatusReducer.js
+++ client/src/store/reducers/StatusReducer.js
@@ -1,6 +1,7 @@
 import { OK, LOADING, ERROR } from '../actions/actionTypes';
 
 // statusReducer 는 너무 단순하네 ..
-const statusReducer = (state, action) => action.type;
+
+const statusReducer = (state, action) => ({ ...state, status: action.type });
 
 export default statusReducer;
