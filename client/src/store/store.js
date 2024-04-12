322e58f7b7cc85a65c57a3e2ed3a8e12100c86b9
unknown <ttddcc119@naver.com>
2024-04-12 20:01:02 +0900

    테마 버튼 기능 완성

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -2,5 +2,5 @@ import { createStore } from 'redux';
 import rootReducer from './rootReducer';
 
 const store = createStore(rootReducer);
-
+console.log(store.getState((state) => state));
 export default store;
