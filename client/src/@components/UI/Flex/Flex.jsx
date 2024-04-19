4f8e475721dd8e45bf2d117c1f178dcf7975a44e
unknown <ttddcc119@naver.com>
2024-04-19 10:02:50 +0900

    flexRow , flexColumn 여분 props 는 rest 처리

--- client/src/@components/UI/Flex/Flex.jsx
+++ client/src/@components/UI/Flex/Flex.jsx
@@ -1,27 +1,21 @@
 import moduleStyle from './Flex.module.css';
 import React from 'react';
-const FlexColumn = ({ children, width, flexGrow = [1], gap = '16px' }) => {
-  if (flexGrow.reduce((pre, cur) => pre + cur) !== 1) {
-    throw new Error('flexGrow 의 합은 항상 1이여야 합니다');
-  }
+
+const FlexColumn = ({ children, flexRatio = [1], ...props }) => {
   return (
-    <section style={{ width, gap }} className={moduleStyle.flexColumn}>
+    <section style={{ ...props }} className={moduleStyle.flexColumn}>
       {React.Children.map(children, (child, index) => {
-        return React.cloneElement(child, { flexGrow: flexGrow[index] });
+        return React.cloneElement(child, { flexGrow: flexRatio[index] });
       })}
     </section>
   );
 };
 
-const FlexRow = ({ children, width, flexGrow = [1], gap = '2%' }) => {
-  if (flexGrow.reduce((pre, cur) => pre + cur) !== 1) {
-    throw new Error('flexGrow 의 합은 항상 1이여야 합니다');
-  }
-
+const FlexRow = ({ children, flexRatio = [1], ...props }) => {
   return (
-    <section style={{ width, gap }} className={moduleStyle.flexRow}>
+    <section style={{ ...props }} className={moduleStyle.flexRow}>
       {React.Children.map(children, (child, index) => {
-        return React.cloneElement(child, { flexGrow: flexGrow[index] });
+        return React.cloneElement(child, { flexGrow: flexRatio[index] });
       })}
     </section>
   );
--- client/src/pages/MenuPage/MenuPage.jsx
+++ client/src/pages/MenuPage/MenuPage.jsx
@@ -6,9 +6,13 @@ import MainCard from '../../@components/Composite/Cards/MainCard';
 const MenuPage = () => {
   return (
     <section className={moduleCss.menu}>
-      <FlexRow flexGrow={[0.5, 0.5]}>
+      <FlexRow flexRatio={[0.5, 0.5]}>
         <MainCard />
-        <MainCard />
+        <FlexColumn>
+          <MainCard />
+          <MainCard />
+          <MainCard />
+        </FlexColumn>
       </FlexRow>
     </section>
   );
